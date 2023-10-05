dc8de4458bb8026ec60284257d48c0a76b1671a7
Maloney18 <lekansolomon18@gmail.com>
2023-10-05 22:15:01 +0100

    another update

--- src/components/propDetails.jsx
+++ src/components/propDetails.jsx
@@ -43,24 +43,15 @@ const PropDetails = () => {//index of the current image
     // console.log(backImg)
 
     return (
-        <div className='p-2.5 h-screen bg-gray-100 flex flex-col gap-y-5'>
-            <div className='relative gap-y-3 pt-10 flex flex-col md:flex-row span md:items-center gap-x-2.5 '>
+        <div className='p-2.5 h-screen bg-gray-100 md:p-5 flex flex-col gap-y-5 md:gap-y-10'>
+            <div className='relative gap-y-5 pt-10  flex flex-col md:flex-row span md:items-end gap-x-5 md:h-60vh'>
                 <div className='flex items-center gap-1.5  cursor-pointer absolute left-0 top-2.5' onClick={() => navigate('/')}>
                     <AiOutlineArrowLeft />
-                    <p className='font-bold'>Back</p>
+                    <p className='font-bold md:text-lg'>Back</p>
                 </div>
 
-                <div className='relative flex flex-col h-1/2 gap-y-5 md:w-2/5 md:h-full'>
+                <div className='relative gap-y-5 md:w-3/5 md:h-full h-50vh'>
                     <img className='h-full w-full rounded-xl' loading='lazy' src={backImg.currentImage} alt={name} />
-
-                    <div className='flex items-center justify-between w-full'>
-                        <p className='bg-white p-2 rounded-lg'>{availability}</p>
-
-                        <div className={` p-2.5 rounded-full max-w-max ${favorite ? 'bg-blue-800' :'bg-white'} cursor-pointer`} onClick={() => dispatch(toggleFavorite(id))}>
-                            <AiOutlineHeart className={`fill-current ${favorite ? 'text-white' :'text-blue-800'}`}/>
-                        </div>
-                    </div>
-
                     {
                         image.length === 1 
                         ? 
@@ -78,54 +69,66 @@ const PropDetails = () => {//index of the current image
                     }
                     {
                         popular &&
-                        <p className='popular absolute top-2.5 left-0 p-2.5 h-10'>
+                        <p className='bg-blue-800 text-white font-semibold absolute rounded-r-xl top-2.5 left-0 p-2.5 md:p-3 md:text-lg'>
                             Popular
                         </p>
                     }
                 </div>
 
-                <div className='flex flex-col gap-5 w-95 self-center md:self-start md:w-3/5 md:h-1/2'>
-                    <div className='flex gap-2.5 items-center'>
-                        <MdOutlineLocationOn className='fill-current text-yellow-800'/> 
-                        <p>{address}</p>
-                    </div>
+                <div className='flex flex-col gap-y-7 md:justify-end md:gap-y-10 w-95 self-center md:h-full md:w-2/5'>
+                    <div className='flex items-center justify-between w-full'>
+                        <p className='bg-white p-2 md:text-lg rounded-lg'>{availability}</p>
 
-                    <p className='font-semibold text-xl'>{name}</p>
-
-                    <div className='flex justify-between items-center md:grid md:grid-cols-2 md:gap-5'>
-                        <div className='icon'>
-                            <BsBuilding className='fill-current text-black'/>
-                            <p>{`${rooms} Rooms(s)`}</p>
-                        </div>
-
-                        <div className='icon'>
-                            <LiaBedSolid className='fill-current text-black'/>
-                            <p>{`${beds} Bed(s)`}</p>
-                        </div>
-
-                        <div className='icon'>
-                            <LiaBathSolid className='fill-current text-black'/>
-                            <p>{`${bathroom} Bath(s)`}</p>
-                        </div>
-
-                        <div className='icon'>
-                            <LiaArrowsAltSolid className='fill-current text-black'/>
-                            <p>{size}</p>
+                        <div className={` p-2.5 shadow-2xl rounded-full max-w-max ${favorite ? 'bg-blue-800' :'bg-white'} cursor-pointer`} onClick={() => dispatch(toggleFavorite(id))}>
+                            <AiOutlineHeart className={`fill-current ${favorite ? 'text-white' :'text-blue-800'}`}/>
                         </div>
                     </div>
 
-                    <div className='flex items-center justify-between'>
-                        <p className='text-xl font-semibold text-blue-800'>{price} <span className='text-sm text-black font-normal'>/month</span></p>
+                    
+                    <div className='flex flex-col md:justify-end gap-5 md:gap-7'>
 
-                        <button className='read-more'>
-                            Read More
-                        </button>
+                        <div className='flex gap-2.5 items-center'>
+                            <MdOutlineLocationOn className='fill-current text-yellow-800 md:h-5 md:w-5'/> 
+                            <p className='md:text-xl'>{address}</p>
+                        </div>
+
+                        <p className='font-semibold text-2xl'>{name}</p>
+
+                        <div className='flex justify-between items-center md:grid md:grid-cols-2 md:gap-5'>
+                            <div className='icon items-center md:items-start'>
+                                <BsBuilding className='fill-current text-black'/>
+                                <p className=''>{`${rooms} Rooms(s)`}</p>
+                            </div>
+
+                            <div className='icon items-center md:items-start'>
+                                <LiaBedSolid className='fill-current text-black'/>
+                                <p>{`${beds} Bed(s)`}</p>
+                            </div>
+
+                            <div className='icon items-center md:items-start'>
+                                <LiaBathSolid className='fill-current text-black'/>
+                                <p>{`${bathroom} Bath(s)`}</p>
+                            </div>
+
+                            <div className='icon items-center md:items-start'>
+                                <LiaArrowsAltSolid className='fill-current text-black'/>
+                                <p>{size}</p>
+                            </div>
+                        </div>
+
+                        <div className='flex items-center justify-between'>
+                            <p className='text-xl font-semibold text-blue-800 md:text-2xl'>{price} <span className='text-sm md:text-md text-black font-normal'>/month</span></p>
+
+                            <button className='read-more md:text-lg'>
+                                Rent Now
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
 
 
-            <div className='flex flex-col gap-y-2.5'>
+            <div className='flex flex-col gap-y-2.5 h-1/2'>
                 <p className='font-black text-xl md:text-3xl'>Description</p>
                 
                 <p> 
